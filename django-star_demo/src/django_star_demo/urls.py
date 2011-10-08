4cdce6c5c79f7b05be46745fadf760ad91cee315
giginet <giginet.net@gmail.com>
2011-10-08 11:24:14 +0900

    sample blog entry display on index.

--- django-star_demo/src/django_star_demo/urls.py
+++ django-star_demo/src/django_star_demo/urls.py
@@ -1,15 +1,24 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
-from django.views.generic import TemplateView
 from django.contrib.auth import views as auth_views
+from django.views.generic import TemplateView
+from blogs.models import Entry
 import os
 
 admin.autodiscover()
-  
+
+class IndexView(TemplateView):
+    template_name = r'index.html'
+
+    def get_context(self, *args, **kwargs):
+        context = super(IndexView, self).get_context(*args, **kwargs)
+        context['object'] = Entry.objects.get(pk=1)
+        return context
+
 urlpatterns = patterns('',
     (r'^admin/',                       include(admin.site.urls)),
     (r'^blogs/',                       include('django_star_demo.blogs.urls')),
-    (r'^api/star/',                 include('star.api.urls')),
+    (r'^api/star/',                    include('star.api.urls')),
     url(r'^$',                         TemplateView.as_view(template_name="index.html"), name='index'),
 )
 
